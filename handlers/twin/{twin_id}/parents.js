2f7f766f9a98c8abec06a5b10b257fde239b06a9
Said El Mallouki <4653505+smallouki@users.noreply.github.com>
2018-03-15 19:35:12 +0100

    no message

--- handlers/twin/{twin_id}/parents.js
+++ handlers/twin/{twin_id}/parents.js
@@ -10,16 +10,12 @@ const log = require('../../../lib/LogHelper').getLogger();
 module.exports = {
     get: function twin_parent_retrieve(req, res, next) {
         log.debug('Retrieving the parents of twin with id: ', req.params.twin_id);
-
         let status = 200;
         db.queryAllTwinParents(req.params.twin_id).then(function (result) {
-            res.json(result);
             res.status(status).send(result);
-            next();
         }).catch(function (error) {
             // error
-            res.json(error);
-            next(error);
+            res.status(500).send(error);
         });
     }
 }
